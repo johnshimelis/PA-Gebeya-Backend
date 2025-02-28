7cd1fdecb1cb7a870dd25554381b188dff03f836
John Shimelis <johnshimelis09@gmail.com>
2025-02-28 05:35:55 +0300

    update

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // Allow all domains
+    credentials: true, // Allow cookies and authentication headers
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
+
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(express.urlencoded({ extended: true })); // Parses URL-encoded form data
+
+// Connect to MongoDB
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/pa-gebeya";
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to PA-Gebeya API 🚀");
+});
+
+// Import Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+const categoryRoutes = require("./routes/categoryRoutes");
+app.use("/api/categories", categoryRoutes);
+
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
+
+const orderRoutes = require("./routes/orderRoutes");
+app.use("/api/orders", orderRoutes);
+
+const cartRoutes = require("./routes/cartRoutes");
+app.use("/api/cart", cartRoutes);
+
+const userRoute = require('./routes/userRoutes');
+app.use('/api/users', userRoute);
+
+const adsRoutes = require("./routes/adsRoutes");
+app.use("/api/ads", adsRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
