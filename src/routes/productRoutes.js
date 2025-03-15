76a2a74e1bdfcdb22545d8825df37fa90518ab8b
John Shimelis <johnshimelis09@gmail.com>
2025-03-15 10:34:59 +0300

    update

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -32,13 +32,14 @@ const upload = multer({
 router.post("/", upload.single("image"), productController.createProduct);
 router.get("/", productController.getAllProducts);
 router.get("/discounted", productController.getDiscountedProducts);
-router.get("/bestsellers", productController.getBestSellers); // New Route
-router.get("/nondiscount", productController.getNonDiscountedProducts); // New Route
+router.get("/bestsellers", productController.getBestSellers);
+router.get("/nondiscount", productController.getNonDiscountedProducts);
 router.get("/:id", productController.getProductById);
+router.get("/category/:categoryId", productController.getProductsByCategory); // New Route
 router.put("/:id", upload.single("image"), productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 
-// Error Handling Middle  ware for Multer
+// Error Handling Middleware for Multer
 router.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
     return res.status(400).json({ message: err.message });
