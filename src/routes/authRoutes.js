ba1263d499c675813bb8afee51e3c1d16b3b2544
John Shimelis <johnshimelis09@gmail.com>
2025-04-01 17:55:40 +0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, verifyOTP } = require("../controllers/authController");
+const { registerUser, loginUser, verifyOTP, requestOTP } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/verify-otp", verifyOTP);  
+router.post("/request-otp", requestOTP); 
 
 module.exports = router;
