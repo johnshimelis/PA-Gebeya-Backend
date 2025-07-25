905895bad87dcc58257fad93f97c796c3729d84f
John Shimelis <johnshimelis09@gmail.com>
2025-07-25 10:57:44 +0300

    updated on backend

--- src/routes/adsRoutes.js
+++ src/routes/adsRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../middlewares/upload"); // <-- add this
+
 const {
   uploadAd,
   getAds,
   deleteAd,
   updateAd,
-  upload, // Multer-S3 middleware
 } = require("../controllers/adsController");
 
-// Ads Routes
-router.post("/:type", upload.array("images", 5), uploadAd);     // Upload new ad
-router.get("/:type", getAds);                                   // Fetch ads of a specific type
-router.delete("/:id", deleteAd);                                // Delete ad and images
-router.put("/:id", upload.array("images", 5), updateAd);        // Update ad with new images
+router.post("/:type", upload.array("images", 5), uploadAd);
+router.get("/:type", getAds);
+router.delete("/:id", deleteAd);
+router.put("/:id", upload.array("images", 5), updateAd);
 
 module.exports = router;
