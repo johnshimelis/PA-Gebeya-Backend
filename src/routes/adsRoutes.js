71e14b638d4b49a317df8ec0795855341f25e2e8
John Shimelis <johnshimelis09@gmail.com>
2025-07-25 09:02:31 +0300

    Update adsRoutes.js

--- src/routes/adsRoutes.js
+++ src/routes/adsRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const adsController = require("../controllers/adsController");
 
-// Routes using S3 upload middleware
-router.post("/:type", adsController.upload.array("images", 5), adsController.uploadAd);
+// Multer Configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+});
+const upload = multer({ storage });
+
+// Routes
+router.post("/:type", upload.array("images", 5), adsController.uploadAd);
 router.get("/:type", adsController.getAds);
 router.delete("/:id", adsController.deleteAd);
-router.put("/:id", adsController.upload.array("images", 5), adsController.updateAd);
+router.put("/:id", upload.array("images", 5), adsController.updateAd);
 
 module.exports = router;
